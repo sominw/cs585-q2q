2f0c2e1576cbc58996c8045fbe61c877cf735d8c
Somin Wadhwa <sominwadhwa@wpa069.wpanet.cs.local>
2019-12-19 18:42:02 -0500

    enc-dec

---
+++ src/utils.py
@@ -0,0 +1,128 @@
+from __future__ import unicode_literals, print_function, division
+from io import open
+import unicodedata
+import string
+import re
+import random
+import time
+import math
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+
+
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+
+class Lang:
+    def __init__(self, name):
+        self.name = name
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "SOS", 1: "EOS"}
+        self.n_words = 2
+
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            self.addWord(word)
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
+            
+
+def normalizeString(s):
+    s = unicodeToAscii(s.lower().strip())
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    return s
+
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+def readLangs(query, ques, reverse=False):
+    print("Reading lines...")
+
+    lines = open('../data/train_q2q.txt', encoding='utf-8').read().strip().split('\n')
+
+    pairs = [[normalizeString(s) for s in l.split('\t')] for l in lines]
+    if reverse:
+        pairs = [list(reversed(p)) for p in pairs]
+        input_lang = Lang(ques)
+        output_lang = Lang(query)
+    else:
+        input_lang = Lang(query)
+        output_lang = Lang(ques)
+
+    return input_lang, output_lang, pairs
+
+
+def prepareData(reverse=False):
+    input_lang, output_lang, pairs = readLangs('query', 'ques',reverse)
+    print("Read %s sentence pairs" % len(pairs))
+    print("Counting words...")
+    for pair in pairs:
+        try:
+            input_lang.addSentence(pair[0])
+            output_lang.addSentence(pair[1])
+        except:
+            print (pair)
+    print("Counted words:")
+    print(input_lang.name, input_lang.n_words)
+    print(output_lang.name, output_lang.n_words)
+    return input_lang, output_lang, pairs
+
+
+def indexesFromSentence(lang, sentence):
+    return [lang.word2index[word] for word in sentence.split(' ')]
+
+
+def tensorFromSentence(lang, sentence):
+    indexes = indexesFromSentence(lang, sentence)
+    indexes.append(EOS_token)
+    return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
+
+
+def tensorsFromPair(pair):
+    input_tensor = tensorFromSentence(input_lang, pair[0])
+    target_tensor = tensorFromSentence(output_lang, pair[1])
+    return (input_tensor, target_tensor)
+
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
+
+def showPlot(points):
+    plt.figure(figsize=(10,10))
+    fig, ax = plt.subplots()
+    ax.set_ylabel('Loss')
+    ax.set_xlabel('Iterations')
+    ax.set_title('Loss Curve')
+    loc = ticker.MultipleLocator(base=0.2)
+    ax.yaxis.set_major_locator(loc)
+    plt.plot(points)
+    fig.savefig('loss_curve.png', format='png', dpi=300)
+    
+
+
